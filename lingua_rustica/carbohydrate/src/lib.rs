90eda6da886e71c85e3eda381cbc323e0a16d7c9
Yaw Boakye <wheresyaw@gmail.com>
2023-06-02 11:05:14 +0100

    initialize carbohydrate crate

---
+++ lingua_rustica/carbohydrate/src/lib.rs
@@ -0,0 +1,4 @@
+// learning about rust structs by implementing a structure
+// for carbohydrate following the description/specification
+// found here:
+// https://chem.libretexts.org/Courses/University_of_Kentucky/UK%3A_CHE_103_-_Chemistry_for_Allied_Health_(Soult)/Chapters/Chapter_5%3A_Properties_of_Compounds/5.2%3A_Carbohydrate_Structures?readerView
